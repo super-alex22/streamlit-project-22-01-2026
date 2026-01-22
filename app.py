9a76a6234a90816e88f135bca69c8f06ed80b76a
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2026-01-22 10:39:11 +0200

    Fix typo in name validation condition

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ name = st.text_input("Enter name")
 if st.button("Check"):
   if name.strip()=="":
     st.warning("I'd like to kindly have you asked to enter text")
-  elif not name is alpha():
+  elif not name.isalpha():
     st.warning("Warning")
   else:
     st.success("The text has been entered correctly")
