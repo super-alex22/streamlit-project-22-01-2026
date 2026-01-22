410c09dcd42bb7799ee37bb731dbee3a681974f2
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2026-01-22 10:36:15 +0200

    Add basic login system with name validation
    Implement a basic login system with name input and validation.

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Login system")
+name = st.text_input("Enter name")
+if st.button("Check"):
+  if name.strip()=="":
+    st.warning("I'd like to kindly have you asked to enter text")
+  elif not name is alpha():
+    st.warning("Warning")
+  else st.success("The text has been entered correctly")
