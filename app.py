f86c89bc5152b8bbb5cc3cce727b86bbf4bdc762
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2026-01-22 10:49:42 +0200

    Add name check for age verification

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.title("Login system")
 name = st.text_input("Enter name")
 age = st.number_input("Enter age")
 if st.button("Check"):
-  if age<18:
+  if age<18 and name="":
     st.error("You are not of legal age to use this program")
   if name.strip()=="":
     st.warning("I'd like to kindly have you asked to enter text")
